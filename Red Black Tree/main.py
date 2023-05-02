f1a9e2b7f048499eecee692416c7f76735d734df
Boris Shapovalov <bashap@bk.ru>
2023-05-02 19:19:30 +0300

    improve

--- Red Black Tree/main.py
+++ Red Black Tree/main.py
@@ -33,183 +33,266 @@
 
 # root.print_tree()
 
-class Node: 
+class RedBlackNode:
 
-    def __init__(self, data, parent=None):
-        self.data = data
+    def __init__(self, key, color='RED'):
+        self.key = key
+        self.color = color
         self.left = None
         self.right = None
-        self.parent = parent
-        self.color = 1
+        self.parent = None
 
-    def search(self, val):
-        if self.data == val:
-            return self
-        if self.data > val:
-            return self.left.search(val) if self.left else None
-        else:
-            return self.right.search(val) if self.right else None
+class RedBlackTree:
+    def __init__(self):
+        self.nil = RedBlackNode(None, 'BLACK')
+        self.root = self.nil
 
-    def print_tree(self):
-            if self.left:
-            self.left.print_tree()
-            print(self.data)
-            if self.right:
-            self.right.print_tree()
-    
-    def insert(self, data):
-    new_node = Node(data)
+    def insert(self, key):
+        """
+        Вставляет новый узел в красно-черное дерево с заданным ключом
+        """
+        # Создаем новый узел
+        new_node = RedBlackNode(key)
 
-    # Perform BST insertion
-    if self.root is None:
-        # Case 1: Empty tree
-        self.root = new_node
-        self.root.color = Node.BLACK
-        return
-    else:
-        # Find the insertion point
-        parent = None
+        # Устанавливаем его потомком равным nil
+        new_node.left = self.nil
+        new_node.right = self.nil
+
+        # Устанавливаем корень как родитель нового узла
+        new_node.parent = self.root
+        # Ищем место для вставки нового узла
         current = self.root
-        while current is not None:
+        parent = None
+        while current != self.nil:
             parent = current
-            if new_node.data < current.data:
+            if key < current.key:
                 current = current.left
             else:
                 current = current.right
 
-        # Insert the new node
+        # Устанавливаем родителя нового узла
         new_node.parent = parent
-        if new_node.data < parent.data:
+
+        # Вставляем новый узел в дерево
+        if parent == None:
+            self.root = new_node
+        elif key < parent.key:
             parent.left = new_node
         else:
             parent.right = new_node
 
-    # Fix the tree
-    self.fix_insert(new_node)
+        # Балансируем дерево
+        self.__fix_insert(new_node)
 
-
-    def fix_violation(self):
-    while self.parent and self.parent.color == 1:
-        if self.parent == self.parent.parent.left:
-            uncle = self.parent.parent.right
-
-            if uncle and uncle.color == 1:
-                self.parent.color = 0
-                uncle.color = 0
-                self.parent.parent.color = 1
-                self = self.parent.parent
+    def __fix_insert(self, new_node):
+        """
+        Балансирует красно-черное дерево после вставки нового узла
+        """
+        while new_node.parent.color == 'RED':
+            if new_node.parent == new_node.parent.parent.left:
+                # Родитель находится на левой ветке
+                y = new_node.parent.parent.right
+                if y.color == 'RED':
+                    # Случай 1: дядя нового узла - красный
+                    new_node.parent.color = 'BLACK'
+                    y.color = 'BLACK'
+                    new_node.parent.parent.color = 'RED'
+                    new_node = new_node.parent.parent
+                else:
+                    if new_node == new_node.parent.right:
+                        # Случай 2: новый узел - правый потомок
+                        new_node = new_node.parent
+                        self.__left_rotate(new_node)
+                    # Случай 3: новый узел - левый потомок
+                    new_node.parent.color = 'BLACK'
+                    new_node.parent.parent.color = 'RED'
+                    self.__right_rotate(new_node.parent.parent)
             else:
-                if self == self.parent.right:
-                    self = self.parent
-                    self.left_rotate()
-                self.parent.color = 0
-                self.parent.parent.color = 1
-                self.parent.parent.right_rotate()
+                # Родитель находится на правой ветке
+                y = new_node.parent.parent.left
+                if y.color == 'RED':
+                    # Случай 1: дядя нового узла - красный
+                    new_node.parent.color = 'BLACK'
+                    y.color = 'BLACK'
+                    new_node.parent.parent.color = 'RED'
+                    new_node = new_node.parent.parent
+                else:
+                    if new_node == new_node.parent.left:
+                        # Случай 2: новый узел - левый потомок
+                        new_node = new_node.parent
+                        self.__right_rotate(new_node)
+                    # Случай 3: новый узел - правый потомок
+                    new_node.parent.color = 'BLACK'
+                    new_node.parent.parent.color = 'RED'
+                    self.__left_rotate(new_node.parent.parent)
+
+        # Корень всегда должен быть черным
+        self.root.color = 'BLACK'
+
+    def delete(self, key):
+        """
+        Удаляет узел с заданным ключом из красно-черного дерева
+        """
+        # Ищем узел для удаления
+        current = self.root
+        while current != self.nil and current.key != key:
+            if key < current.key:
+                current = current.left
+            else:
+                current = current.right
+
+        # Узел не найден
+        if current == self.nil:
+            return
+
+        # Если удаляемый узел имеет не более одного потомка, просто заменяем его на потомка
+        if current.left == self.nil or current.right == self.nil:
+            child = current.left if current.left != self.nil else current.right
+            if current.parent == None:
+                self.root = child
+            elif current == current.parent.left:
+                current.parent.left = child
+            else:
+                current.parent.right = child
+            child.parent = current.parent
+
+            # Если удаляемый узел был черным, возможно, нарушается баланс дерева
+            if current.color == 'BLACK':
+                self.__fix_delete(child)
         else:
-            uncle = self.parent.parent.left
+            # Удаляемый узел имеет двух потомков, находим его преемника и заменяем им удаляемый узел
+            successor = self.__tree_minimum(current.right)
+            current.key = successor.key
+            self.delete(successor)
 
-            if uncle and uncle.color == 1:
-                self.parent.color = 0
-                uncle.color = 0
-                self.parent.parent.color = 1
-                self = self.parent.parent
-            else:
-                if self == self.parent.left:
-                    self = self.parent
-                    self.right_rotate()
-                self.parent.color = 0
-                self.parent.parent.color = 1
-                self.parent.parent.left_rotate()
-
-    self.color = 0
-    while self.parent:
-        self = self.parent
-
-        def left_rotate(self):
-            new_root = self.right
-            self.right = new_root.left
-            if new_root.left:
-                new_root.left.parent = self
-                new_root.parent = self.parent
-            if not self.parent:
-                root = new_root
-            elif self == self.parent.left:
-                self.parent.left = new_root
-            else:
-                self.parent.right = new_root
-                new_root.left = self
-                self.parent = new_root
-
-
-    def fix_insert(self, node):
-    while node.parent is not None and node.parent.color == RED:
-        if node.parent == node.parent.parent.left:
-            uncle = node.parent.parent.right
-            if uncle is not None and uncle.color == RED:
-                # Case 1: Parent and uncle are both red
-                node.parent.color = BLACK
-                uncle.color = BLACK
-                node.parent.parent.color = RED
-                node = node.parent.parent
-            else:
-                if node == node.parent.right:
-                    # Case 2: Parent is red and uncle is black, and node is a right child
+    def __fix_delete(self, node):
+        """
+        Балансирует красно-черное дерево после удаления узла
+        """
+        while node != self.root and node.color == 'BLACK':
+            if node == node.parent.left:
+                # Узел находится на левой ветке
+                sibling = node.parent.right
+                if sibling.color == 'RED':
+                    # Случай 1: брат узла - красный
+                    sibling.color = 'BLACK'
+                    node.parent.color = 'RED'
+                    self.__left_rotate(node.parent)
+                    sibling = node.parent.right
+                if sibling.left.color == 'BLACK' and sibling.right.color == 'BLACK':
+                    # Случай 2: оба потомка брата - черные
+                    sibling.color = 'RED'
                     node = node.parent
-                    self.rotate_left(node)
-                # Case 3: Parent is red and uncle is black, and node is a left child
-                node.parent.color = BLACK
-                node.parent.parent.color = RED
-                self.rotate_right(node.parent.parent)
+                else:
+                    if sibling.right.color == 'BLACK':
+                        # Случай 3: левый потомок брата - красный, правый - черный
+                        sibling.left.color = 'BLACK'
+                        sibling.color = 'RED'
+                        self.__right_rotate(sibling)
+                        sibling = node.parent.right
+                    # Случай 4: правый потомок брата - красный
+                    sibling.color = node.parent.color
+                    node.parent.color = 'BLACK'
+                    sibling.right.color = 'BLACK'
+                    self.__left_rotate(node.parent)
+                    node = self.root.right
+            else:
+                # Узел находится на правой ветке
+                sibling = node.parent.left
+                if sibling.color == 'RED':
+                    # Случай 1: брат узла - красный
+                    sibling.color = 'BLACK'
+                    node.parent.color = 'RED'
+                    self.__right_rotate(node.parent)
+                    sibling = node.parent.left
+                if sibling.left.color == 'BLACK' and sibling.right.color == 'BLACK':
+                    # Случай 2: оба потомка брата - черные
+                    sibling.color = 'RED'
+                    node = node.parent
+                else:
+                    if sibling.left.color == 'BLACK':
+                        # Случай 3: правый потомок брата - красный, левый - черный
+                        sibling.right.color = 'BLACK'
+                        sibling.color = 'RED'
+                        self.__left_rotate(sibling)
+                        sibling
+                        sibling = node.parent.left
+                    # Случай 4: левый потомок брата - красный
+                    sibling.color = node.parent.color
+                    node.parent.color = 'BLACK'
+                    sibling.left.color = 'BLACK'
+                    self.__right_rotate(node.parent)
+                    node = self.root.left
+
+        node.color = 'BLACK'
+
+    def __tree_minimum(self, node):
+        """
+        Находит узел с минимальным значением ключа в поддереве, корнем которого является заданный узел
+        """
+        while node.left != self.nil:
+            node = node.left
+        return node
+
+    def __left_rotate(self, node):
+        """
+        Осуществляет левый поворот вокруг заданного узла
+        """
+        right_child = node.right
+        node.right = right_child.left
+        if right_child.left != self.nil:
+            right_child.left.parent = node
+        right_child.parent = node.parent
+        if node.parent == None:
+            self.root = right_child
+        elif node == node.parent.left:
+            node.parent.left = right_child
         else:
-            uncle = node.parent.parent.left
-            if uncle is not None and uncle.color == RED:
-                # Case 4: Parent and uncle are both red, and node is a right child
-                node.parent.color = BLACK
-                uncle.color = BLACK
-                node.parent.parent.color = RED
-                node = node.parent.parent
-            else:
-                if node == node.parent.left:
-                    # Case 5: Parent is red and uncle is black, and node is a left child
-                    node = node.parent
-                    self.rotate_right(node)
-                # Case 6: Parent is red and uncle is black, and node is a right child
-                node.parent.color = BLACK
-                node.parent.parent.color = RED
-                self.rotate_left(node.parent.parent)
-    self.root.color = BLACK
+            node.parent.right = right_child
+        right_child.left = node
+        node.parent = right_child
 
-def right_rotate(self):
-    new_root = self.left
-    self.left = new_root.right
-    if new_root.right:
-        new_root.right.parent = self
-        new_root.parent = self.parent
-    if not self.parent:
-        root = new_root
-    elif self == self.parent.right:
-        self.parent.right = new_root
-    else:
-        self.parent.left = new_root
-        new_root.right = self
-        self.parent = new_root
+    def __right_rotate(self, node):
+        """
+        Осуществляет правый поворот вокруг заданного узла
+        """
+        left_child = node.left
+        node.left = left_child.right
+        if left_child.right != self.nil:
+            left_child.right.parent = node
+        left_child.parent = node.parent
+        if node.parent == None:
+            self.root = left_child
+        elif node == node.parent.right:
+            node.parent.right = left_child
+        else:
+            node.parent.left = left_child
+        left_child.right = node
+        node.parent = left_child
 
-root = Node(10)
-root.left = Node(5)
-root.right = Node(15)
-root.left.left = Node(3)
-root.left.right = Node(8)
-root.right.left = Node(12)
-root.right.right = Node(20)
+rbt = RedBlackTree()
 
-root.print_tree()
+# Вставляем элементы в дерево
+rbt.insert(10)
+rbt.insert(20)
+rbt.insert(30)
+rbt.insert(100)
+rbt.insert(90)
+rbt.insert(40)
 
-root.insert(9)
-root.insert(7)
-root.insert(13)
-root.insert(18)
+# Печатаем элементы дерева в отсортированном порядке
+print("Элементы дерева в отсортированном порядке: ", end="")
+rbt.inorder_traversal(rbt.root)
+print()
 
-root.print_tree()
+# Удаляем элементы из дерева
+rbt.delete(10)
+rbt.delete(20)
+
+# Печатаем элементы дерева в отсортированном порядке
+print("Элементы дерева в отсортированном порядке: ", end="")
+rbt.inorder_traversal(rbt.root)
+print()
 
 
     
